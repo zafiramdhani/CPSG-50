4f6eaad1d2df4f0eb7034cd81f2be6840532e8b8
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-05-27 19:32:47 +0700

    Ubah article #tes ke section #home

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,7 @@ import '../styles/style.css';
 import StartApp from './templates/template';
 
 const home = new StartApp({
-  content: document.querySelector('#tes'),
+  content: document.querySelector('#home'),
 });
 
 window.addEventListener('hashchange', () => {
