f8d2290c1b0af46d383977189e344bde46fd0234
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-06-05 20:34:26 +0700

    Ubah chart menjadi img

--- src/scripts/views/start-test.js
+++ src/scripts/views/start-test.js
@@ -2,7 +2,7 @@ const StartTest = {
   render() {
     return `
       <div class="start-test_wrapper">
-        <iframe width="100%" height="1000" src="https://visionsource.com/site/assets/files/1192/vision_source_eye_chart.pdf#toolbar=0&zoom=110"></iframe>
+        <img class="snellen-chart" src="https://i.ibb.co/k10TJtG/vision-source-eye-chart-1.png" alt="Snellen chart">
         <p>This Snellen chart is obtained from <a href="https://visionsource.com/patients/free-eye-chart-download/" target="_blank" rel="noopener noreferrer">Visionsource.com</a></p>
       </div>
     `;
--- src/scripts/views/tips-content.js
+++ src/scripts/views/tips-content.js
@@ -1,6 +1,6 @@
 const tipsContent = {
   whatIsEyetest: 'EYETEST adalah sebuah website tes visus mata atau ketelitian penglihatan menggunakan Snellen chart secara online dan mandiri. EYETEST ditargetkan bagi user umum ataupun bagi user yang selalu bekerja menatap layar gadget/monitor selama berjam-jam setiap harinya dan ingin mengetahui kondisi ketelitian penglihatan mereka tanpa perlu membeli Snellen chart sendiri dan cukup diakses secara online.',
-  howToUseEyetest: 'Dari menu Tes, klik "Coba Sekarang" dan baca aturan sebelum melakukan tes. Jika sudah, klik "Lanjutkan" dan lakukan tes hingga selesai sampai muncul hasil tes anda. Riwayat tes akan disimpan dalam menu Riwayat dan dapat dihapus sesuai kebutuhan anda.',
+  howToUseEyetest: 'Dari menu Tes, klik "Coba Sekarang" dan lakukan tes hingga selesai sampai muncul hasil tes anda. Riwayat tes akan disimpan dalam menu Riwayat dan dapat dihapus sesuai kebutuhan anda.',
 };
 
 export default tipsContent;
