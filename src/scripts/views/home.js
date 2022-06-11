b6e8e7636d8c0ef2d35fa00a12932c89538e81bf
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-06-11 10:37:23 +0700

    Memperbaiki tips dropdown

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import './navbar-config';
+import { Modal } from 'bootstrap';
 import tipsContent from './tips-content';
 import bg from '../public/hero-image2.png';
 import dollarIcon from '../public/dollar-regular-60s.png';
@@ -119,41 +120,6 @@ const Home = {
           </div>
         </div>
       </section>
-        
-
-      <!--
-      <section class="tips container" id="tips">
-        <div class="tips-title">
-          <h3>Tips</h3>
-        </div>
-        <div class="tips-wrapper">
-          <a class="btn btn-light" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Apa itu EYETEST ?</a>
-          <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-              ${tipsContent.whatIsEyetest}
-            </div>
-          </div>
-        </div>
-
-        <div class="tips-wrapper">
-          <a class="btn btn-light" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">Bagaimana cara penggunaan EYETEST ?</a>
-          <div class="collapse" id="collapseExample2">
-            <div class="card card-body">
-              ${tipsContent.howToUseEyetest}
-            </div>
-          </div>
-        </div>
-
-        <div class="tips-wrapper">
-          <a class="btn btn-light" data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">Bagaimana cara menjaga kesehatan mata ?</a>
-          <div class="collapse" id="collapseExample3">
-            <div class="card card-body">
-              Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-            </div>
-          </div>
-        </div>
-      </section>
-      -->
     `;
   },
 };
