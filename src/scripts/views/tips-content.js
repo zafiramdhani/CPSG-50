2dfc4123325a16aa211f06fb5061059d8c954442
Siti Maryam <sitimaryam.cmps@gmail.com>
2022-06-12 14:29:47 +0700

    Update tips

--- src/scripts/views/tips-content.js
+++ src/scripts/views/tips-content.js
@@ -1,6 +1,7 @@
 const tipsContent = {
   whatIsEyetest: 'EYETEST adalah sebuah website tes visus mata atau ketelitian penglihatan menggunakan Snellen chart secara online dan mandiri. EYETEST ditargetkan bagi user umum ataupun bagi user yang selalu bekerja menatap layar gadget/monitor selama berjam-jam setiap harinya dan ingin mengetahui kondisi ketelitian penglihatan mereka tanpa perlu membeli Snellen chart sendiri dan cukup diakses secara online.',
   howToUseEyetest: 'Dari menu Tes, klik "Coba Sekarang" dan lakukan tes hingga selesai sampai muncul hasil tes anda. Riwayat tes akan disimpan dalam menu Riwayat dan dapat dihapus sesuai kebutuhan anda.',
+  howToMaintainEyeHealth: 'Jagalah kesehatan mata anda dengan Makan makanan yang sehat, Hindari rokok, Gunakan kacamata hitam dengan UV protector, Istirahatkan mata secara berkala. <i>sumber : kemenkes.go.id</i> ',
 };
 
 export default tipsContent;
