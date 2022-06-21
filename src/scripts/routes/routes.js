2b328ae7affc05b7e9c2668113ca192d0f57e105
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-06-21 16:56:40 +0700

    Menambah route riwayat

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,11 @@
 import Home from '../views/home';
+import Riwayat from '../views/riwayat';
 import StartTest from '../views/start-test';
 
 const routes = {
   '/': Home,
   '/start-test': StartTest,
+  '/riwayat': Riwayat,
 };
 
 export default routes;
