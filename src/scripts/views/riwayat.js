c3d6747ff6294a02c0857ed95d076de32ec089d1
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2022-06-22 10:47:46 +0700

    Menambah page riwayat

---
+++ src/scripts/views/riwayat.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-plusplus */
+import { arrTest } from './start-test';
+
+const Riwayat = {
+  render() {
+    return `
+      <div class="history-container container mt-5 mb-5 pt-5" style="min-height: 100vh;">
+        <table class="table table-striped table-hover mt-5">
+          <thead>
+            <tr class="text-center">
+              <th scope="col"><i class="fa-solid fa-hashtag"></i>&nbsp;\tNo</th>
+              <th scope="col"><i class="fa-solid fa-calendar"></i>&nbsp;\tTanggal</th>
+              <th scope="col"><i class="fa-solid fa-star"></i>&nbsp;\tSkor</th>
+              <th scope="col" class="text-center"><i class="fa-solid fa-pen-to-square"></i>&nbsp;\tAksi</th>
+            </tr>
+          </thead>
+          <tbody class="align-middle text-center">
+            ${arrTest.map((test, index) => `
+              <tr>
+                <th scope="row">${index + 1}</th>
+                <td>${test.id}</td>
+                <td>${test.result}</td>
+                <td><button type="button" class="btn btn-danger" id="btn-delete" value="${index}"><i class="fa-solid fa-trash"></i></button></td>
+              </tr>
+            `).join('')}
+          </tbody>
+        </table>
+      </div>
+    `;
+  },
+
+  afterRender() {
+    const btnDelete = document.getElementsByClassName('btn-danger');
+    for (let i = 0; i < btnDelete.length; i++) {
+      btnDelete[i].addEventListener('click', () => {
+        arrTest.splice(btnDelete[i].value, 1);
+        localStorage.setItem('EYETEST', JSON.stringify(arrTest));
+        window.location.reload();
+      });
+    }
+  },
+};
+
+export default Riwayat;
